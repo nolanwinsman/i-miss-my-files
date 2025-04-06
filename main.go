c037e7c54088442c553a33525b7feac41e69d729
nolanwinsman <nolanwinsman@gmail.com>
2025-04-05 19:33:18 -0700

    added timer to main

--- main.go
+++ main.go
@@ -4,11 +4,26 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 var m map[int64]string
 
+// timer returns a function that prints the name argument and
+// the elapsed time between the call to timer and the call to
+// the returned function. The returned function is intended to
+// be used in a defer statement:
+//
+//	defer timer("sum")()
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", name, time.Since(start))
+	}
+}
+
 func main() {
+	defer timer("main")()
 	d := `C:\Users\nolan\Desktop\shows`
 	m = make(map[int64]string)
 
